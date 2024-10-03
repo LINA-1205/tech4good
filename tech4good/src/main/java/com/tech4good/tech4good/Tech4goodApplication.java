68381b6e7f3d7ebc51b522e3f52b3ff8e4e3b937
Lina <mi_marce1@hotmail.com>
2024-10-03 17:44:24 -0500

    se actuazlia y avanza en el trabajo final

---
+++ tech4good/src/main/java/com/tech4good/tech4good/Tech4goodApplication.java
@@ -0,0 +1,13 @@
+package com.tech4good.tech4good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4goodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4goodApplication.class, args);
+	}
+
+}
---
+++ tech4good/src/test/java/com/tech4good/tech4good/Tech4goodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech4good.tech4good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4goodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
